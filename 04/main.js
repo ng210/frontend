1dbca7afdec78c125dfd5e9d5b4cb3043288b75e
ng210 <gabor.nemeth.de@gmail.com>
2025-09-24 12:47:55 +0200

    táblás játék példa

---
+++ 04/main.js
@@ -0,0 +1,46 @@
+var _grid = null;
+var _selectedCell = null;
+
+function onCellEnter(e) {
+    let cell = e.target;
+    cell.classList.add('lit');
+}
+
+function onCellLeave(e) {
+    let cell = e.target;
+    cell.classList.remove('lit');
+}
+
+function onCellClick(e) {
+    let cell = e.target;
+    if (_selectedCell != null) {
+        _selectedCell.classList.remove('selected');
+    }
+    _selectedCell = cell;
+    cell.classList.add('selected');
+
+}
+
+function createGrid(size) {
+    grid = document.createElement('div');
+    grid.className = 'grid';
+    grid.style.display = 'grid';
+    grid.style.gridTemplate = `repeat(${size}, 1fr)/repeat(${size}, 1fr)`;
+    grid.style.width = 12 * size + 'px';
+    grid.style.height = 12 * size + 'px';
+    for (let gi=0; gi<size; gi++) {
+        for (let gj=0; gj<size; gj++) {
+            let cell = document.createElement('div');
+            //cell.innerHTML = gi+'-'+gj;
+            cell.addEventListener('pointerenter', onCellEnter);
+            cell.addEventListener('pointerleave', onCellLeave);
+            cell.addEventListener('click', onCellClick);
+            grid.appendChild(cell);
+        }
+    }
+    document.body.appendChild(grid);
+}
+
+window.addEventListener('load', () => {
+    createGrid(8);
+});
